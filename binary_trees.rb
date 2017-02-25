443cf1a7f9a2d868e45e436b016d201cdc128e78
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-02-25 09:07:35 -0500

    Created Node.add, Node.to_s, make_tree(arr), though at least one of them isn't working right.

--- binary_trees.rb
+++ binary_trees.rb
@@ -1,9 +1,44 @@
 class Node
-	attr_accessor :val, :left, :right, :parent
-	def initialize(value, left, right, parent)
+	attr_accessor :val, :parent, :left, :right
+	def initialize(value, parent, left=nil, right=nil)
 		@val = value
 		@left = left
 		@right = right
 		@parent = parent
-	end	
+	end
+
+	def add(item)
+		#there must be a better way to do this
+		if @val == nil
+			@val = item
+		elsif @val >= item
+			if @left == nil
+				@left = Node.new(item, self)
+			else
+				@left.add(item)
+			end
+		else
+			if @right == nil
+				@left = Node.new(item, self)
+			else
+				@right.add(item)
+			end
+		end
+	end
+
+	def to_s
+		"#{@val} (#{self.left.to_s}) (#{self.right.to_s})"
+	end
 end
+
+def make_tree(arr)
+	root = Node.new(nil, nil)
+	arr.each do |item|
+		root.add(item)			
+	end
+	return root
+end
+
+
+t = make_tree([1, 2, 4, 3, 6, 5, 7])
+puts t.to_s
