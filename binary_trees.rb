a8cacfee7d113dd19dfe7f01be7df903fdcbf64e
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-02-25 09:33:04 -0500

    Created Node.breadth_first_search, which seems to work but needs further testing; changed node.to_s to only show the node's value and created Node.show_tree which shows all of a node's descendants as well.

--- binary_trees.rb
+++ binary_trees.rb
@@ -27,7 +27,26 @@ class Node
 	end
 
 	def to_s
-		"#{@val} (#{self.left.to_s}) (#{self.right.to_s})"
+		"Node with #{@val}"
+	end
+
+	def show_tree
+	 	"#{@val}(#{self.left.to_s}) (#{self.right.to_s})"
+	end
+
+	def breadth_first_search(item)
+		queue = []
+		result = nil
+		if @val = item
+			result = self
+		else
+			queue += @left if @left
+			queue += @right if @right
+			queue.each do |node|
+				result = node.breadth_first_search(item)
+			end
+		end
+		return result
 	end
 end
 
@@ -40,5 +59,5 @@ def make_tree(arr)
 end
 
 
-t = make_tree([1, 2, 4, 3, 6, 5, 7])
-puts t.to_s
+t = make_tree([11, 1, 1, 3, 2, 2, 0, -12, -15, -9])
+puts t.breadth_first_search(-9)
