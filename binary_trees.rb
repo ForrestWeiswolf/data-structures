3556c21c76a2dabc15cb49bd06a9b812e9b4030b
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-02-26 09:40:32 -0500

    fix to show_tree

--- binary_trees.rb
+++ binary_trees.rb
@@ -31,7 +31,15 @@ class Node
 	end
 
 	def show_tree
-	 	"#{@val}(#{self.left.to_s}) (#{self.right.to_s})"
+		result = "#{@val}"
+		[@left, @right].each do |subtree|
+			if subtree
+				result += " (#{subtree.show_tree})"
+			else
+				result += " (nil)"
+			end
+		end
+		return result
 	end
 
 	def breadth_first_search(item)
@@ -60,4 +68,5 @@ end
 
 
 t = make_tree([11, 1, 1, 3, 2, 2, 0, -12, -15, -9])
+puts t.show_tree
 puts t.breadth_first_search(-9)
