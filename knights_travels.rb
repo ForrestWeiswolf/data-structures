cd0109999f4dfbb70c93d98816f028890fd8e1f5
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-03-08 09:51:13 -0500

    created method to list the positions a knight can move to from a given square

--- knights_travels.rb
+++ knights_travels.rb
@@ -1,2 +1,16 @@
 def knight_moves(from, to)
 end
+
+def next_positions(pos)
+	result = []
+	moves = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
+	moves.each do |move|
+		possibility = [pos[0] + move[0], pos[1] + move[1]]
+		if possibility[0].between?(0, 7) && possibility[1].between?(0, 7)
+			result << possibility
+		end
+	end
+	return result
+end
+
+next_positions([0, 0]).each { |e|  puts e.to_s}
